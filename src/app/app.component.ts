c15cd67c6e7dca5d9a8778fcfb49d7a2bfe0a907
mcarcelen <mcarcelen@uees.edu.ec>
2024-06-04 18:15:23 -0500

    Commit RecursosService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { TablaComponent } from './shared/tabla/tabla.component';
+import { RecursosService } from './servicios/recursos.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Foto } from './interfaz/foto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactoComponent, TablaComponent],
+  imports: [RouterOutlet, ContactoComponent, TablaComponent, HttpClientModule],
+  providers: [RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'clienteAngular';
+  fotos: Foto[] = [];
+
+  constructor(private recursosService: RecursosService){
+    recursosService.obtenerDatos().subscribe(respuesta => {
+      this.fotos = respuesta as Array<Foto>
+    });
+  }
 }
